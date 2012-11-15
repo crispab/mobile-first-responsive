7a5943e0a667545abbb8a1b6495ac523d8dc4f6c
Daniel Sundman <daniel@yds.se>
2012-11-15 23:09:34 +0100

    include the first static html content as slide 0

--- js/index.js
+++ js/index.js
@@ -9,18 +9,31 @@
 			this.title = data.title;
 			this.templatedTitle = $.render.titleTemplate({title:this.title});
 			this.url = data.url;
+			if (data.selector) {
+				this.text = $(data.selector).text();
+			}
+		};
+		Index.prototype._renderSlide = function(sel, data) {
+			console.log('markup: ' + data);
+			$(sel).html(data);
+			$('head title').text(this.templatedTitle);
 		};
 		Index.prototype.toString = function() {
-			return 'Index(' + this.title + ', ' + this.url + ')';
+			return 'Index(' + this.title + ', ' + this.url + ', ' + this.selector +')';
 		};
 		Index.prototype.load = function(sel) {
-			var that = this, title = $('head title');
+			var that = this;
 			console.log('load: ' + this.toString());
-			title.text('Loading ' + this.title + '...');
-			$.get(this.url, function(data) {
-				$(sel).html(data);
-				title.text(that.templatedTitle);
-			});
+			$('head title').text('Loading ' + this.title + '...');
+			if (this.text) {
+				console.log('markup: ' + this.text);
+				this._renderSlide(sel, this.text);
+			} else {
+				$.get(this.url, function(data) {
+					that._renderSlide(sel, data);
+					that.text = data;
+				});
+			}
 		};
 		return Index;
 	});
